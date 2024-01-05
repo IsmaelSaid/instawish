31ca5b46b2b8e830628d5c478c6c5f536911785d
Said Ismael <ismael.said@protonmail.com>
2024-01-05 08:17:55 +0100

    Ajout liste des contacts

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,42 @@ void main() {
                         style: TextStyle(color: Colors.white)),
                   ),
                 ]),
-            actions:  [
-              const Padding(
+            actions: const [
+              Padding(
                 padding: EdgeInsets.only(right: 8.0),
                 child: CircleAvatar(
-                  backgroundImage: AssetImage("assets/avatar.jpg"),
-                ),
-              ),
-              TextButton(
-                child: const Icon(Icons.add_box, color: Colors.white),
-                onPressed: (){}
+                    backgroundImage: AssetImage("assets/avatar.jpg")),
               ),
             ],
             backgroundColor: Colors.cyan[300]),
+        body: Container(
+          height: 100,
+          color: Colors.black12,
+          margin: const EdgeInsets.only(top: 10),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (var i = 0; i < 5; i++)
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          shape: BoxShape.circle),
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: const CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage("assets/avatar.jpg"),
+                      ),
+                    ),
+                    Text("User $i")
+                  ],
+                ),
+            ],
+          ),
+        ),
       ),
     ),
   );
